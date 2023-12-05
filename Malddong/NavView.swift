84c3b30b6139f1fdf56a4125d0d3ef9ca436f34a
jake5113 <jongwon5113@gmail.com>
2023-12-05 17:34:09 +0900

    toilet, tourist

---
+++ Malddong/NavView.swift
@@ -0,0 +1,18 @@
+//
+//  NavView.swift
+//  Malddong
+//
+//  Created by 이종원 on 12/4/23.
+//
+
+import SwiftUI
+
+struct NavView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavView()
+}
